40c454d0ef2a97a3b83fedb0888cc68e5b1c4f89
alebqa <alexandra.brehuescu@gmail.com>
2018-02-09 20:50:41 +0200

    test

--- src/main/java/tests/BaseTest.java
+++ src/main/java/tests/BaseTest.java
@@ -1,4 +1,45 @@
 package tests;
 
+import enums.Browsers;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import sun.jvmstat.monitor.Units;
+
+import java.io.File;
+import java.util.concurrent.TimeUnit;
+
 public class BaseTest {
+
+    public WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() throws InterruptedException {
+        driver = WebBrowsers.getDriver((Browsers.CHROME);
+        driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+        driver.get("stage.drawpoint.com");
+
+
+        Thread.sleep(1000);
+    }
+
+
+    public void quit() {
+        driver.quit();
+    }
+
+    File[] getListOfFiles(String directoryName) {
+        ClassLoader classLoader = getClass().getClassLoader();
+        File directory = new File(classLoader.getResource(directoryName).getFile());
+        File[] files = directory.listFiles();
+        // System.out.println("Found " + files.length + " files in " + directoryName + " folder");
+        return files;
+    }
+
+    @AfterMethod
+    public void closeBrowser() throws InterruptedException{
+        driver.quit();
+    }
 }
+
